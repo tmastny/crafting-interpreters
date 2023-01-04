8abfbfb3e26bd19349c623d1f5fc8b4ecf348de8
Tim Mastny <tim.mastny@wave.com>
2023-01-03 19:38:18 -0600

    save

--- lox/Environment.java
+++ lox/Environment.java
@@ -1,10 +1,20 @@
 package lox;
 
+import java.security.spec.EncodedKeySpec;
 import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+  final Environment enclosing;
   private final Map<String, Object> values = new HashMap<>();
+
+  Environment() {
+    enclosing = null;
+  }
+
+  Environment(Environment enclosing) {
+    this.enclosing = enclosing;
+  }
 
   void define(String name, Object value) {
     values.put(name, value);
@@ -15,12 +25,19 @@ class Environment {
       return values.get(name.lexeme);
     }
 
+    if (enclosing != null) return enclosing.get(name);
+
     throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
   }
 
   void assign(Token name, Object value) {
     if (values.containsKey(name.lexeme)) {
       values.put(name.lexeme, value);
+      return;
+    }
+
+    if (enclosing != null) {
+      enclosing.assign(name, value);
       return;
     }
 
