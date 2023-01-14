cb88f060835134f5f6c8f7ecaa39e755152cca56
Tim Mastny <tim.mastny@wave.com>
2023-01-14 10:26:51 -0600

    save

--- clox/chunk.c
+++ clox/chunk.c
@@ -8,6 +8,7 @@ void initChunk(Chunk* chunk) {
   chunk->capacity = 0;
   chunk->code = NULL;
   chunk->lines = NULL;
+  chunk->linecap = 0;
   initValueArray(&chunk->constants);
 }
 
@@ -16,13 +17,23 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
-  chunk->lines[chunk->count] = line;
   chunk->count++;
+  writeLine(chunk, line);
 }
+
+void writeLine(Chunk* chunk, int line) {
+  if (chunk->linecap < line + 1) {
+    int oldCapacity = chunk->linecap;
+    chunk->linecap = GROW_CAPACITY(oldCapacity);
+    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->linecap);
+  }
+
+  chunk->lines[line]++;
+}
+
 
 int addConstant(Chunk* chunk, Value value) {
   writeValueArray(&chunk->constants, value);
--- clox/chunk.h
+++ clox/chunk.h
@@ -14,12 +14,14 @@ typedef struct {
   int capacity;
   uint8_t* code;
   int* lines;
+  int linecap;
   ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
+void writeLine(Chunk* chunk, int line);
 int addConstant(Chunk* chunk, Value value);
 
 #endif
