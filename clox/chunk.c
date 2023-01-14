90dfceaf644a487814a57a5a35bf39b90ba6dd36
Tim Mastny <tim.mastny@wave.com>
2023-01-14 10:07:21 -0600

    save

--- clox/chunk.c
+++ clox/chunk.c
@@ -24,6 +24,25 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
   chunk->count++;
 }
 
+void writeConstant(Chunk* chunk, Value value, int line) {
+  int constant = addConstant(chunk, value);
+
+  if (constant < 256) {
+    writeChunk(chunk, OP_CONSTANT, line);
+    writeChunk(chunk, constant, line);
+    return;
+  }
+
+  writeChunk(chunk, OP_CONSTANT_LONG, line);
+
+  int mask = 0xff0000;
+  uint8_t byte;
+  for (int i = 0; i <= 16; i += 8) {
+    byte = (constant & (mask >> i)) >> (16 - i);
+    writeChunk(chunk, byte, line);
+  }
+}
+
 int addConstant(Chunk* chunk, Value value) {
   writeValueArray(&chunk->constants, value);
   return chunk->constants.count - 1;
--- clox/debug.c
+++ clox/debug.c
@@ -11,6 +11,18 @@ void disassembleChunk(Chunk* chunk, const char* name) {
   }
 }
 
+static int constantLongInstruction(const char* name, Chunk* chunk, int offset) {
+
+  int constant = chunk->code[offset + 1] << 16;
+  constant |= chunk->code[offset + 2] << 8;
+  constant |= chunk->code[offset + 3];
+
+  printf("%-16s %4d '", name, constant);
+  printValue(chunk->constants.values[constant]);
+  printf("'\n");
+  return offset + 4;
+}
+
 static int constantInstruction(const char* name, Chunk* chunk, int offset) {
   uint8_t constant = chunk->code[offset + 1];
   printf("%-16s %4d '", name, constant);
@@ -36,6 +48,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   switch (instruction) {
     case OP_CONSTANT:
       return constantInstruction("OP_CONSTANT", chunk, offset);
+    case OP_CONSTANT_LONG:
+      return constantLongInstruction("OP_CONSTANT_LONG", chunk, offset);
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
 
