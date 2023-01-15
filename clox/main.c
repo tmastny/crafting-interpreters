29f58d1b0a4f6255ecc4585bc3a6477b482d8824
Tim Mastny <tim.mastny@wave.com>
2023-01-15 13:30:31 -0600

    save

--- clox/main.c
+++ clox/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
@@ -9,6 +11,7 @@ int main(int argc, const char* argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
+  printf("(1.2 + 3.4) / 5.6\n");
   int constant = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
@@ -27,9 +30,107 @@ int main(int argc, const char* argv[]) {
   writeChunk(&chunk, OP_NEGATE, 123);
 
   writeChunk(&chunk, OP_RETURN, 123);
-
-  // disassembleChunk(&chunk, "test chunk");
   interpret(&chunk);
+  freeChunk(&chunk);
+  initChunk(&chunk);
+
+  // #2
+  freeChunk(&chunk);
+  initChunk(&chunk);
+
+  printf("1 * 2 + 3\n");
+  constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+
+  constant = addConstant(&chunk, 3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+  writeChunk(&chunk, OP_RETURN, 123);
+  interpret(&chunk);
+  freeChunk(&chunk);
+  initChunk(&chunk);
+
+  // #3
+  printf("1 + 2 * 3\n");
+  constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+  constant = addConstant(&chunk, 3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+  writeChunk(&chunk, OP_ADD, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+  interpret(&chunk);
+  freeChunk(&chunk);
+  initChunk(&chunk);
+
+  // #4
+  printf("3 - 2 - 1\n");
+  constant = addConstant(&chunk, 3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_SUBTRACT, 123);
+
+  constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_SUBTRACT, 123);
+  writeChunk(&chunk, OP_RETURN, 123);
+  interpret(&chunk);
+  freeChunk(&chunk);
+  initChunk(&chunk);
+
+  // #5
+  printf("1 + 2 * 3 - 4 / -5\n");
+  constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+  constant = addConstant(&chunk, 3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+  writeChunk(&chunk, OP_ADD, 123);
+
+  constant = addConstant(&chunk, 4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  constant = addConstant(&chunk, 5);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_NEGATE, 123);
+  writeChunk(&chunk, OP_DIVIDE, 123);
+  writeChunk(&chunk, OP_SUBTRACT, 123);
+  writeChunk(&chunk, OP_RETURN, 123);
+  interpret(&chunk);
+  freeChunk(&chunk);
+  initChunk(&chunk);
+
+
   freeVM();
   freeChunk(&chunk);
   return 0;
