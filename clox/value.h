f9222f2c2d227a6554a7f24bb277f41181f81a64
Tim Mastny <tim.mastny@wave.com>
2023-01-18 16:18:30 -0600

    save

--- clox/value.h
+++ clox/value.h
@@ -26,7 +26,7 @@ typedef struct {
 
 #define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
 #define NIV_VAL           ((Value){VAL_NIL, {.number = 0}})
-#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 typedef struct {
   int capacity;
