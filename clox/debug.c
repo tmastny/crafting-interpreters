d73fd9c24ff1ead68001d1e387ddae7942a63187
Tim Mastny <tim.mastny@wave.com>
2023-01-18 18:08:22 -0600

    save

--- clox/debug.c
+++ clox/debug.c
@@ -42,6 +42,12 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return simpleInstruction("OP_TRUE", offset);
     case OP_FALSE:
       return simpleInstruction("OP_FALSE", offset);
+    case OP_EQUAL:
+      return simpleInstruction("OP_EQUAL", offset);
+    case OP_GREATER:
+      return simpleInstruction("OP_GREATER", offset);
+    case OP_LESS:
+      return simpleInstruction("OP_LESS", offset);
     case OP_ADD:
       return simpleInstruction("OP_ADD", offset);
     case OP_SUBTRACT:
@@ -50,12 +56,6 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return simpleInstruction("OP_MULTIPLY", offset);
     case OP_DIVIDE:
       return simpleInstruction("OP_DIVIDE", offset);
-    case OP_EQUAL:
-      return simpleInstruction("OP_EQUAL", offset);
-    case OP_GREATER:
-      return simpleInstruction("OP_GREATER", offset);
-    case OP_LESS:
-      return simpleInstruction("OP_LESS", offset);
     case OP_NOT:
       return simpleInstruction("OP_NOT", offset);
     case OP_NEGATE:
