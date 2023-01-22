5d65bc0191c3200529bc82523870b8f78583abbd
Tim Mastny <tim.mastny@wave.com>
2023-01-22 17:59:01 -0600

    save

--- clox/vm.c
+++ clox/vm.c
@@ -76,6 +76,8 @@ static void concatenate() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_SHORT() \
+    (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
     do { \
@@ -195,6 +197,7 @@ static InterpretResult run() {
     }
   }
 #undef READ_BYTE
+#undef READ_SHORT
 #undef READ_CONSTANT
 #undef READ_STRING
 #undef BINARY_OP
