7b34cd3e3625249c3a231c5949a20a9a38ec9b5c
Tim Mastny <tim.mastny@wave.com>
2023-01-25 18:51:19 -0600

    save

--- README.md
+++
@@ -1,4 +0,0 @@
-Without register:
-9.22746e+06
-3.17521
-With register:
--- bench.sh
+++ bench.sh
@@ -1,9 +1,9 @@
 gcc clox/*.c
 
-echo "Without register:" >> README.md
+echo "With register:" >> README.md
 ./a.out clox/clock.lox >> README.md
 
 
-gcc clox-register/*.c
-echo "With register:" >> README.md
+gcc clox-standard/*.c
+echo "Without register:" >> README.md
 ./a.out clox/clock.lox >> README.md
