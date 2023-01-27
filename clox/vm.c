f63e5bcb262357ecc93a80b906e99a9b307974b2
Tim Mastny <tim.mastny@wave.com>
2023-01-26 19:09:20 -0600

    save

--- clox/vm.c
+++ clox/vm.c
@@ -346,6 +346,7 @@ static InterpretResult run() {
         break;
       case OP_RETURN: {
         Value result = pop();
+        closeUpvalues(frame->slots);
         vm.frameCount--;
         if (vm.frameCount == 0) {
           pop();
